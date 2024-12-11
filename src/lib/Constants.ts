89b78150d731ac1df027ed686c10b553e537e691
Yash Agrawal <yash4agr@gmail.com>
2024-12-11 21:12:05 +0530

    bugs

--- src/lib/Constants.ts
+++ src/lib/Constants.ts
@@ -1,4 +1,4 @@
-// export const ArticleEndPoint = 'https://dev.to/api/articles';
-// export const UserInfoEndpoint = 'https://dev.to/api/articles?username=';
-// export const MediumFeedEndpoint = 'https://medium.com/feed/';
+export const ArticleEndPoint = 'https://dev.to/api/articles';
+export const UserInfoEndpoint = 'https://dev.to/api/articles?username=';
+export const MediumFeedEndpoint = 'https://medium.com/feed/';
 export const Email = 'yash4agr@gmail.com';
